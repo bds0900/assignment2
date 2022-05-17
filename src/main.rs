c6e389a81110d235f1153b66af1ea4534279e2db
Doosan <doosan@hartmann-controls.com>
2022-05-17 19:23:22 -0400

    delete commented code

--- src/main.rs
+++ src/main.rs
@@ -45,40 +45,4 @@ fn main() {
         };
     }
 }
-// fn menu() {
-//     println!("1. Change base");
-//     println!("2. Change exponent");
-//     println!("3. Display base raised to exponent");
-//     println!("4. Exit program");
-// }
 
-// fn check_range(val: i32, min: i32, max: i32) -> i32 {
-//     let mut ret = 0;
-//     if val < min || val > max {
-//         ret = 0;
-//     } else {
-//         ret = 1;
-//     }
-//     ret
-// }
-// fn calculate_power(base: i32, exp: i32) -> i32 {
-//     if exp > 1 {
-//         return base * calculate_power(base, exp - 1);
-//     } else {
-//         return base;
-//     }
-
-// }
-// fn get_num(mut input: &mut String)->i32 {
-//     println!("Enter an integer number");
-//     use std::io;
-//     io::stdin()
-//         .read_line(&mut input)
-//         .expect("Failed to read line");
-
-//     let input: i32 = match input.trim().parse() {
-//         Ok(num) => num,
-//         Err(_) => 0,
-//     };
-//     input
-// }
